c9c4ea5190921bcfd2ddec7233be4e903c694514
Matas Kazanavicius <mataskazanavicius@Matass-MacBook-Pro.local>
2018-10-28 16:49:33 +0200

    initial commit

--- front/src/stores/CarStore.jsx
+++ front/src/stores/CarStore.jsx
@@ -1,1 +1,20 @@
 // čia bus mašinų store'as
+import { observable, action, computed } from "mobx";
+
+class CarStore {
+  @observable
+  cars = [];
+
+  @action
+  addCar = car => {
+    this.cars.push(car);
+  };
+
+  @computed
+  get carsCount() {
+    return this.cars.length;
+  }
+}
+
+const store = new CarStore();
+export default store;
