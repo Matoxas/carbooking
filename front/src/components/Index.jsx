dcee38d46268b3a6ed846f02155b296291d9c336
Matas Kazanavicius <mataskazanavicius@Matass-MacBook-Pro.local>
2018-10-29 23:35:38 +0200

    index

--- front/src/components/Index.jsx
+++ front/src/components/Index.jsx
@@ -4,7 +4,7 @@ import "../style/index.css";
 const Index = () => {
   return (
     <div className="container padding-top">
-      <div className="flex flex-center fullHeight">
+      <div className="flex flex-center fullHeight text-center">
         <h2>Čia bus pagrindinis paieškos laukas</h2>
       </div>
     </div>
